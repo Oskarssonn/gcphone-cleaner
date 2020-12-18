1f096b311a940fda651708cebccda1fccbe846b4
Oskarssonn <74346220+Oskarssonn@users.noreply.github.com>
2020-12-18 12:25:04 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Oskarsson'
+
+server_scripts {
+	"@mysql-async/lib/MySQL.lua",
+	"scripti.lua"
+}
---
+++ scripti.lua
@@ -0,0 +1,6 @@
+MySQL.ready(function ()
+    MySQL.ASync.execute('DELETE FROM phone_messages WHERE time < (NOW() = interval 5 DAY)', {})
+    MySQL.ASync.execute('DELETE FROM phone_calls WHERE time < (NOW() = interval 5 DAY)', {})
+    MySQL.ASync.execute('DELETE FROM phone_app_chat WHERE time < (NOW() = interval 5 DAY)', {})
+    MySQL.ASync.execute('DELETE FROM twitter_tweets WHERE time < (NOW() = interval 3 DAY)', {})
+end)
